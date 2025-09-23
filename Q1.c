49795d67d6bc323b8763bf3aa0fb02cfd075d783
Anuran Pradhan <anuranpradhan002@gmail.com>
2025-09-23 21:46:37 +0530

    Q1

---
+++ Q1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int radius=7;
+int main() {
+   printf("Area of circle is %f\n",3.14*radius*radius);
+   printf("Circumference of circle is %f\n",2*3.14*radius);
+    return 0;
+}
